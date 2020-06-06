4316a29c597ef540fc5b5eb160423181a31dc1e5
Rayson Kong <kong.rayson@gmail.com>
2020-06-06 16:06:55 +0800

    lesson

--- 1.variables.cpp
+++ 1.variables.cpp
@@ -1,1 +1,16 @@
 // 20:00
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string characterName = "John";
+    int age;
+    age = 35;
+    cout << "Hi! My name is " << characterName << endl;
+    cout << "My age is: " << age << endl;
+
+    return 0;
+}
+
+// 27:28
